ce214b817ba4933a38554c0fd206346712f47893
dharana77 <dharana7723@gmail.com>
2025-03-26 15:58:37 +0900

    feat: init project

---
+++ src/main/java/com/test/jenkins/deployment/TestDeploymentOfJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.test.jenkins.deployment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestDeploymentOfJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestDeploymentOfJenkinsApplication.class, args);
+	}
+
+}
